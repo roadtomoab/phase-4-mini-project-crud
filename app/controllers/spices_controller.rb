b63b6bc6c0041536925da5c4678fb976a93534da
Noah Blumenstein <noah.blumenstein@gmail.com>
2022-01-11 22:05:18 -0500

    finished

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,39 @@
+class SpicesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+    def index
+        render json: Spice.all
+    end
+
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    def update
+        spice = find_spice
+        spice.update(spice_params)
+        render json: spice
+    end
+
+    def destroy
+        spice = find_spice
+        spice.destroy
+        head :no_content
+    end
+
+    private
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+    def find_spice
+        spice = Spice.find_by(id: params[:id])
+    end
+
+    def render_not_found
+        render json: { error: "spice not found" }, status: :not_found
+    end
+
+end
